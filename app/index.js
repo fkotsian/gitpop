0a976f22953d45e5cfea38aa939b739d0e7f8187
Frank Kotsianas <frankkotsianas@gmail.com>
2016-06-23 00:50:32 -0700

    Load bundle in index.html, not index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,7 @@
-require('../public/bundle');
+require('babel-polyfill');
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Application = require('./components/application');
+
+ReactDOM.render(<Application />, document.getElementById('app'));
