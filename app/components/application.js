d64d8c0febbd6ffdbc6cb751cbbfc44ebf9ecc00
Frank Kotsianas <frankkotsianas@gmail.com>
2016-06-23 10:05:09 -0700

    Add navbar to Application

--- app/components/application.js
+++ app/components/application.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const RepoList = require('./repo_list');
 const GithubClient = require('../helpers/github_client');
+const {Navbar, Nav, NavItem} = require('react-bootstrap');
 
 class Application extends React.Component {
   constructor(props) {
@@ -27,8 +28,22 @@ class Application extends React.Component {
   render() {
     return (
       <div>
-        HELLO!
-        <RepoList repoItems={this.state.repoItems} />
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">GitPop</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem>The Top 100 Repos on GitHub</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div id="app-container">
+          <RepoList repoItems={this.state.repoItems} />
+        </div>
       </div>
     );
   };
